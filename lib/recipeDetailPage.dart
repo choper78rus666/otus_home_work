cca5171eaaf72b5db0edd7fc5eef3c05468ddf6c
choper78rus666 <driver-tt@mail.ru>
2022-09-19 23:47:01 +0300

    route

--- lib/recipeDetailPage.dart
+++ lib/recipeDetailPage.dart
@@ -46,21 +46,41 @@ class RecipeDetailPage extends StatelessWidget {
 
   Widget _buildDetail(BuildContext context, index) {
     return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
+      child: Container(
+        color: Colors.white,
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 38),
+          // crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              recipeList[index]['title'] ?? '',
-              style: const TextStyle(
-                fontFamily: 'Roboto',
-                fontSize: 24,
-                height: 1,
-                fontWeight: FontWeight.w500,
+            SizedBox(
+              height: 32,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 32,
+                    child: Text(
+                      recipeList[index]['title'] ?? '',
+                      style: const TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      softWrap: true,
+                    ),
+                  ),
+                  IconButton(
+                    padding: const EdgeInsets.symmetric(horizontal: 23),
+                    icon: Image.asset('assets/icons/heart.png'),
+                    tooltip: 'Избранное',
+                    onPressed: () {},
+                  ),
+                ],
               ),
-              softWrap: true,
             ),
-            Expanded(
+            SizedBox(
+              height: 32,
               child: Row(
                 children: [
                   Icon(
@@ -82,6 +102,37 @@ class RecipeDetailPage extends StatelessWidget {
                 ],
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: Image.asset(
+                recipeList[index]['imagePaths']['detail']
+                    .toString()
+                    .isEmpty
+                    ? 'assets/images/no_image.png'
+                    : recipeList[index]['imagePaths']['detail'],
+                fit: BoxFit.contain,
+                width: double.infinity,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(3, 0, 0, 18),
+              child: Text(
+                'Ингредиенты',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color(0xFF165932),
+                ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(5)),
+                  border: Border.all(color: const Color(0XFF797676), width: 3,)
+              ),
+              //Todo: список ингредиентов
+              child: ListView(),
+            ),
+            const Text('end')
           ],
         ),
       ),
