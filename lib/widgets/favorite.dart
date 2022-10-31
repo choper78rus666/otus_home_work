c9aabf41b981dcc115b6221a5b7a4c097fdd1187
choper78rus <driver-tt@mail.ru>
2022-10-31 19:26:40 +0300

    add rive animation for Favorite

--- lib/widgets/favorite.dart
+++ lib/widgets/favorite.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:home_work/controller/globals.dart';
+import 'package:rive/rive.dart';
 
 // Виджет добавления в избранное
 class Favorite extends StatefulWidget {
@@ -13,6 +14,7 @@ class Favorite extends StatefulWidget {
 
 class _FavoriteState extends State<Favorite> {
   bool _isSelected = false;
+  bool _isAnimated = false;
 
   @override
   Widget build(BuildContext context) {
@@ -22,13 +24,21 @@ class _FavoriteState extends State<Favorite> {
     return IconButton(
       alignment: Alignment.topCenter,
       padding: const EdgeInsets.symmetric(horizontal: 23),
-      icon: Image.asset(_isSelected
-          ? 'assets/icons/heart_active.png'
-          : 'assets/icons/heart.png'),
+      icon: _isSelected
+          ? (_isAnimated // Проверка на запуск анимации, иначе просто иконка
+              ? const RiveAnimation.asset(
+                  'assets/animations/active_heart.riv',
+                  alignment: Alignment.topCenter,
+                  fit: BoxFit.contain,
+                )
+              : Image.asset('assets/icons/heart_active.png'))
+          : Image.asset('assets/icons/heart.png'),
       tooltip: 'Избранное',
       onPressed: () {
         globals.myVariable[widget.index]['is_favorite'] = !_isSelected;
-        setState(() {});
+        setState(() {
+          _isAnimated = true;
+        });
       },
     );
   }
