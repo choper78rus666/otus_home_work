30bedb28662ae41161f22f6f781083b4d16c8c71
choper78rus666 <choper78rus@gmail.com>
2022-12-04 20:29:37 +0300

    bottom navigation

--- lib/widgets/recipients_list.dart
+++ lib/widgets/recipients_list.dart
@@ -13,6 +13,31 @@ class RecipeList extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xffececec),
       body: _buildList(context),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+            icon:  Icon(Icons.local_pizza_outlined),
+            label: 'Рецепты',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Холодильник',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Избранное',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Профиль',
+          ),
+        ],
+        currentIndex: 1,
+        backgroundColor: const Color(0xFFFFFFFF),
+        unselectedItemColor: const Color(0xFFC2C2C2),
+        selectedItemColor: const Color(0xFF2ECC71),
+       // onTap: ()=>{},
+      ),
     );
   }
 
